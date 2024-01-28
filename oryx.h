57c113d91a17646750de1c58a6d3578bc2f5706f
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-28 18:31:51 +0330

    Init: the header file

---
+++ oryx.h
@@ -0,0 +1,12 @@
+#ifndef ORYX_H
+#define ORYX_H
+
+typedef enum ProcessState ProcessState;
+typedef enum ProcessBondage ProcessBondage;
+typedef struct ProcessControlBlock ProcessControlBlock;
+
+
+
+#endif
+
+
