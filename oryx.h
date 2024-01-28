9ef2681e0b5d46aa714fe26c0aca6ed02b8bce4e
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 01:21:17 +0330

    Added: memory page declartions

--- oryx.h
+++ oryx.h
@@ -16,34 +16,46 @@ typedef int registerid_t;
 typedef int exitcode_t;
 typedef unsigned long processnum_t;
 
-ProcessControlBlock create_process_control_block(procid_t parent_id, 
-                char name[MAX_PROCESS_NAME], ProcessBondage bondage);
+ProcessControlBlock create_process_control_block(procid_t parent_id,
+                                                 char name[MAX_PROCESS_NAME],
+                                                 ProcessBondage bondage);
 ShortTermScheduler create_short_term_scheduler(void);
 ShortTermScheduler create_long_term_scheduler(void);
 ProcessScheduler create_process_scheduler(void);
-void process_schedular_add_pcb(ProcessScheduler *sched, ProcessControlBlock *pcb);
+void process_schedular_add_pcb(ProcessScheduler *sched,
+                               ProcessControlBlock *pcb);
 
-typedef struct VirtualMemoryPage VirtualMemoryPage;
+typedef enum MemoryPageLevel MemoryPageLevel;
+typedef struct MemoryPage MemoryPage;
 typedef struct PageTable PageTable;
+typedef struct TLBEntry TLBEntry;
+typedef struct TransLookasideBuffer TransLookasideBuffer;
 typedef unsigned int pageid_t;
 typedef unsigned long physaddr_t;
 typedef unsigned long pageaddr_t;
 typedef unsigned long pagesize_t;
 typedef unsigned long pageoffs_t;
 typedef unsigned long mapaddr_t;
-typedef int pageflag_t;
+typedef unsigned int pageflag_t;
 typedef unsigned long acctime_t;
 typedef unsigned long avail_t;
+typedef unsigned long pagenum_t;
+typedef unsigned long virtpagenum_t;
+typedef unsigned long physpagenum_t;
+typedef int faultstat_t;
 
-
-VirtualMemoryPage create_virtual_page(pageid_t id,                   
-                pageflag_t flags, 
-                physaddr_t physical_addr,   
-                pagesize_t size,   
-                pageoffs_t offset);
-
-
+MemoryPage create_virtual_page(pageid_t id, pageflag_t flags,
+                               physaddr_t physical_addr, pagesize_t size,
+                               pageoffs_t offset);
+TLBEnty create_tlb_entry(virtpagenum_t virtual_page_number,
+                         physpagenum_t physical_page_number);
+TransLookasideBuffer create_tlb(void);
+void add_tlb_entry(TransLookasideBuffer *tlb, virtpagenum_t virtual_page_number,
+                   physpagenum_t physical_page_number);
+MemoryPage create_memory_page(MemoryPageLevel level, pageid_t id,
+                              pageflag_t flags, physaddr_t physical_addr,
+                              pagesize_t size, pageoffs_t offset);
+void add_mempage_imm_table(MemoryPage *page, PageTable *imm_table);
+void add_mempage_tlb(MemoryPage *page, TransLookasideBuffer *tbl);
 
 #endif
-
-
