8d149d09872e867799da08e706fde266f4391d34
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 07:52:02 +0330

    Removed: the scheduler (it was lacky and incomplete)

--- process-control-block.c
+++ process-control-block.c
@@ -47,38 +47,6 @@ struct ProcessControlBlock {
   SchedulingInfo sched_info;
 };
 
-struct ShortTermScheduler {
-  procid_t io_queue[MAX_SHORT_TERM_QUEUE];
-  procid_t cpu_queue[MAX_SHORT_TERM_QUEUE];
-  procid_t waiting[MAX_SHORT_TERM_WAITING];
-  procid_t ready[MAX_SHORT_TERM_READY];
-  procid_t dispatched[MAX_SHORT_TERM_DISPATCHED];
-  queueindex_t io_queue_index;
-  queueindex_t cpu_queue_index;
-  processnum_t last_waiting_index;
-  processnum_t last_ready_index;
-  processnum_t last_dispatched_index;
-};
-
-struct LongTermScheduler {
-  procid_t io_queue[MAX_LONG_TERM_QUEUE];
-  procid_t cpu_quue[MAX_LONG_TERM_QUEUE];
-  pcocid_t waiting[MAX_LONG_TERM_WAIING];
-  procid_t ready[MAX_LONG_TERM_READY];
-  procid_t dispatched[MAX_LONG_TERM_DISPATCHED];
-  queueindex_t io_queue_index;
-  queueindex_t cpu_queue_index;
-  processnum_t last_waiting_index;
-  processnum_t last_ready_index;
-  processnum_t last_dispatched_index;
-};
-
-struct ProcessScheduler {
-  ProcessControlBlock *processes[MAX_PROCESS_NUM];
-  ShortTermSchedular short_term_scheduler;
-  LongTermScheduler long_term_scheduler;
-  processnum_t last_process_index;
-};
 
 ProcessControlBlock create_process_control_block(procid_t parent_id,
                                                  char name[MAX_PROCESS_NAME],
@@ -113,73 +81,4 @@ ProcessControlBlock create_process_control_block(procid_t parent_id,
   pcb.sched_info = create_empty_sched_info();
 
   return pcb;
-}
-
-ShortTermScheduler create_short_term_scheduler(void) {
-  ShortTermScheduler stsched;
-
-  for (int i = 0; i < MAX_SHORT_TERM_QUEUE; i++) {
-    stsched.io_queue[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_SHORT_TERM_QUEUE; i++) {
-    stsched.cpu_queue[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_SHORT_TERM_WITING; i++) {
-    stsched.waiting[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_SHORT_TERM_READY; i++) {
-    stsched.ready[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_SHORT_TERM_DISPATCHED; i++) {
-    stsched.dispatched[i] = 0;
-  }
-
-  return stsched;
-}
-
-ShortTermScheduler create_long_term_scheduler(void) {
-  LongTermScheduler ltsched;
-
-  for (int i = 0; i < MAX_LONG_TERM_QUEUE; i++) {
-    ltsched.io_queue[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_LONG_TERM_QUEUE; i++) {
-    ltsched.cpu_queue[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_LONG_TERM_WITING; i++) {
-    ltsched.waiting[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_LONG_TERM_READY; i++) {
-    ltsched.ready[i] = 0;
-  }
-
-  for (int i = 0; i < MAX_LONG_TERM_DISPATCHED; i++) {
-    ltsched.dispatched[i] = 0;
-  }
-
-  return ltsched;
-}
-
-ProcessScheduler create_process_scheduler(void) {
-  ProcessScheduler sched;
-
-  for (processnum_t i = 0; i < MAX_PROCESS_NUM; i++) {
-    sched.processes[i] = NULL;
-  }
-
-  sched.short_term_scheduler = create_short_term_scheduler();
-  sched.long_term_scheduler = create_long_term_scheduler();
-  sched.last_process_index = 0;
-}
-
-void process_schedular_add_pcb(ProcessScheduler *sched,
-                               ProcessControlBlock *pcb) {
-  sched.processes[sched.last_process_index++] = pcb;
 }
