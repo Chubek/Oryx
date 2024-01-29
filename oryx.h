ec455362b3f78a1910339749244725084af9899b
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 06:34:56 +0330

    Removed: old uselss files

--- oryx.h
+++ oryx.h
@@ -1,6 +1,8 @@
 #ifndef ORYX_H
 #define ORYX_H
 
+// + A: Processes +
+
 typedef enum ProcessState ProcessState;
 typedef enum ProcessBondage ProcessBondage;
 typedef enum ProcessEagerness ProcessEeagerness;
@@ -25,40 +27,21 @@ ProcessScheduler create_process_scheduler(void);
 void process_schedular_add_pcb(ProcessScheduler *sched,
                                ProcessControlBlock *pcb);
 
-typedef enum MemoryPageLevel MemoryPageLevel;
-typedef struct MemoryPage MemoryPage;
-typedef struct PageTable PageTable;
-typedef struct TLBEntry TLBEntry;
-typedef struct TransLookasideBuffer TransLookasideBuffer;
-typedef unsigned int pageid_t;
-typedef unsigned long physaddr_t;
-typedef unsigned long virtaddr_t;
-typedef unsigned long pagesize_t;
-typedef unsigned long pageoffs_t;
-typedef unsigned long mapaddr_t;
-typedef unsigned int pageflag_t;
-typedef unsigned long acctime_t;
-typedef unsigned long avail_t;
-typedef unsigned long pagenum_t;
-typedef unsigned long virtpagenum_t;
-typedef unsigned long physpagenum_t;
-typedef int faultstat_t;
 
-MemoryPage create_virtual_page(pageid_t id, 
-			       pageflag_t flags,
-                               physaddr_t physical_addr,
-			       virtaddr_t virtual_addr,
-			       pagesize_t size,
-                               pageoffs_t offset);
-TLBEnty create_tlb_entry(virtpagenum_t virtual_page_number,
-                         physpagenum_t physical_page_number);
-TransLookasideBuffer create_tlb(void);
-void add_tlb_entry(TransLookasideBuffer *tlb, virtpagenum_t virtual_page_number,
-                   physpagenum_t physical_page_number);
-MemoryPage create_memory_page(MemoryPageLevel level, pageid_t id,
-                              pageflag_t flags, physaddr_t physical_addr,
-                              pagesize_t size, pageoffs_t offset);
-void add_mempage_imm_table(MemoryPage *page, PageTable *imm_table);
-void add_mempage_tlb(MemoryPage *page, TransLookasideBuffer *tbl);
+// + B: Memory +
+
+typedef 
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
