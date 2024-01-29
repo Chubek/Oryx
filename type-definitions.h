f7b45a4912b0499bd4ac2d251a84c8618387b7cc
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 07:52:33 +0330

    Changed: newline at sections

--- type-definitions.h
+++ type-definitions.h
@@ -5,16 +5,15 @@ typedef enum ProcessState ProcessState;
 typedef enum ProcessBondage ProcessBondage;
 typedef enum ProcessEagerness ProcessEeagerness;
 typedef enum ProcessPriority ProcessPriority;
+
 typedef struct ProcessControlBlock ProcessControlBlock;
-typedef struct ShortTermScheduler ShortTermScheduler;
-typedef struct LongTermScheduler LongTermScheduler;
-typedef struct ProcessScheduler ProcessScheduler;
 typedef struct PageTableEntry PageTableEntry;
 typedef struct PageTable PageTable;
 typedef struct PageDirectory PageDirectory;
 typedef struct VirtualPageTableEntry VirtualPageTableEntry;
 typedef struct VirtualPageTable VirtualPageTable;
 typedef struct VirtualPageDirectory VirtualPageDirectory;
+
 typedef struct X64PageTableEntry X64PageTableEntry;
 typedef struct X64PageTable X64PageTable;
 typedef struct X64PageDirectory X64PageDirectory;
