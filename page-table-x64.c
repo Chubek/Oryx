666f61d03f5b8d719bedc37b825f11163780564d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 07:47:54 +0330

    Changed: aesthetic changes

--- page-table-x64.c
+++ page-table-x64.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #define PAGE_SIZE 4096
-#define PAGE_TABLE_ENTRIES 512
+#define X64_PAGE_TABLE_ENTRIES 512
 
 struct X64PageTableEntry {
   uint64_t present : 1;
@@ -20,24 +20,24 @@ struct X64PageTableEntry {
   uint64_t no_execute : 1;
 };
 
-struct PageTable {
-  X64PageTableEntry entries[PAGE_TABLE_ENTRIES];
+struct X64PageTable {
+  X64PageTableEntry entries[X64_PAGE_TABLE_ENTRIES];
 };
 
 struct X64PageDirectoryPointerTable {
-  PageTable *page_tables[PAGE_TABLE_ENTRIES];
+  X64PageTable *page_tables[X64_PAGE_TABLE_ENTRIES];
 };
 
 struct X64PageDirectory {
-  PageTable *page_tables[PAGE_TABLE_ENTRIES];
+  X64PageTable *page_tables[X64_PAGE_TABLE_ENTRIES];
 };
 
-struct X64PaginTable {
-  PageTable *page_tables[PAGE_TABLE_ENTRIES];
-} X64PaginTable;
+struct X64PageTable {
+  X64PageTable *page_tables[X64_PAGE_TABLE_ENTRIES];
+};
 
 struct X64PageMapLevel4 {
-  X64PageDirectoryPointerTable *pdpts[PAGE_TABLE_ENTRIES];
+  X64PageDirectoryPointerTable *pdpts[X64_PAGE_TABLE_ENTRIES];
 };
 
 void initialize_page_table_entry(X64PageTableEntry *entry, uint64_t address,
@@ -49,38 +49,38 @@ void initialize_page_table_entry(X64PageTableEntry *entry, uint64_t address,
   entry->user_accessible = user_accessible;
 }
 
-void initialize_page_table(PageTable *pageTable) {
-  for (int i = 0; i < PAGE_TABLE_ENTRIES; ++i) {
+void initialize_page_table(X64PageTable *pageTable) {
+  for (int i = 0; i < X64_PAGE_TABLE_ENTRIES; ++i) {
     initialize_page_table_entry(&(pageTable->entries[i]), 0, 0, 0, 0);
   }
 }
 
 void initialize_pdpt(X64PageDirectoryPointerTable *pdpt) {
-  for (int i = 0; i < PAGE_TABLE_ENTRIES; ++i) {
+  for (int i = 0; i < X64_PAGE_TABLE_ENTRIES; ++i) {
     pdpt->page_tables[i] = NULL;
   }
 }
 
 void initialize_pd(X64PageDirectory *pd) {
-  for (int i = 0; i < PAGE_TABLE_ENTRIES; ++i) {
+  for (int i = 0; i < X64_PAGE_TABLE_ENTRIES; ++i) {
     pd->page_tables[i] = NULL;
   }
 }
 
-void intialize_pt(X64PaginTable *pt) {
-  for (int i = 0; i < PAGE_TABLE_ENTRIES; ++i) {
+void intialize_pt(X64PageTable *pt) {
+  for (int i = 0; i < X64_PAGE_TABLE_ENTRIES; ++i) {
     pt->page_tables[i] = NULL;
   }
 }
 
 void initialize_pml4(X64PageMapLevel4 *pml4) {
-  for (int i = 0; i < PAGE_TABLE_ENTRIES; ++i) {
+  for (int i = 0; i < X64_PAGE_TABLE_ENTRIES; ++i) {
     pml4->pdpts[i] = NULL;
   }
 }
 
 uint64_t translate_virtual_address(X64PageMapLevel4 *pml4,
-                                   VirtualPageTable *vpt) {
+                                   VirtualX64PageTable *vpt) {
   x64_asm_translate_virtual_address(pml4, vpt->virtual_address);
   x64_map_pml4_to_virtual_page_table(vpt, pml4);
   return 0;
