9004b04073e0156069f3cf541e2166c8d8242ef5
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 17:53:04 +0330

    Added: virtual address structure and methods to get physical address

--- page-tables.d
+++ page-tables.d
@@ -1,87 +1,93 @@
-import std.typecons;
+import std.bitmanip;
 
-enum PAGE_SIZE = 4096;  
+enum PAGE_SIZE = 4096;
 
 
 struct PML4Entry {
-    uint64_t present : 1;      
-    uint64_t readWrite : 1;    
-    uint64_t userSupervisor : 1; 
-    uint64_t writeThrough : 1;  
-    uint64_t cacheDisable : 1;  
-    uint64_t accessed : 1;      
-    uint64_t reserved1 : 1;     
-    uint64_t size : 1;          
-    uint64_t reserved2 : 3;     
-    uint64_t physicalAddress : 40;  
-    uint64_t reserved3 : 11;    
-    uint64_t executeDisable : 1;   
-}
+    ulong present : 1;
+    ulong readWrite : 1;
+    ulong userSupervisor : 1;
+    ulong writeThrough : 1;
+    ulong cacheDisable : 1;
+    ulong accessed : 1;
+    ulong reserved1 : 1;
+    ulong size : 1;
+    ulong reserved2 : 3;
+    ulong physicalAddress : 40;
+    ulong reserved3 : 11;
+    ulong executeDisable : 1;
 
+    ulong getPhysicalAddress() {
+        return physicalAddress << 12;
+    }
+}
 
 struct PDPTEntry {
-    uint64_t present : 1;
-    uint64_t readWrite : 1;
-    uint64_t userSupervisor : 1;
-    uint64_t writeThrough : 1;
-    uint64_t cacheDisable : 1;
-    uint64_t accessed : 1;
-    uint64_t reserved1 : 1;
-    uint64_t largePage : 1;     
-    uint64_t reserved2 : 3;
-    uint64_t physicalAddress : 40;
-    uint64_t reserved3 : 11;
-    uint64_t executeDisable : 1;
-}
+    ulong present : 1;
+    ulong readWrite : 1;
+    ulong userSupervisor : 1;
+    ulong writeThrough : 1;
+    ulong cacheDisable : 1;
+    ulong accessed : 1;
+    ulong reserved1 : 1;
+    ulong largePage : 1;
+    ulong reserved2 : 3;
+    ulong physicalAddress : 40;
+    ulong reserved3 : 11;
+    ulong executeDisable : 1;
 
+    ulong getPhysicalAddress() {
+        return physicalAddress << 12;
+    }
+}
 
 struct PDEntry {
-    uint64_t present : 1;
-    uint64_t readWrite : 1;
-    uint64_t userSupervisor : 1;
-    uint64_t writeThrough : 1;
-    uint64_t cacheDisable : 1;
-    uint64_t accessed : 1;
-    uint64_t reserved1 : 1;
-    uint64_t largePage : 1;
-    uint64_t reserved2 : 3;
-    uint64_t physicalAddress : 40;
-    uint64_t reserved3 : 11;
-    uint64_t executeDisable : 1;
-}
+    ulong present : 1;
+    ulong readWrite : 1;
+    ulong userSupervisor : 1;
+    ulong writeThrough : 1;
+    ulong cacheDisable : 1;
+    ulong accessed : 1;
+    ulong reserved1 : 1;
+    ulong largePage : 1;
+    ulong reserved2 : 3;
+    ulong physicalAddress : 40;
+    ulong reserved3 : 11;
+    ulong executeDisable : 1;
 
+    ulong getPhysicalAddress() {
+        return physicalAddress << 12;
+    }
+}
 
 struct PTEntry {
-    uint64_t present : 1;
-    uint64_t readWrite : 1;
-    uint64_t userSupervisor : 1;
-    uint64_t writeThrough : 1;
-    uint64_t cacheDisable : 1;
-    uint64_t accessed : 1;
-    uint64_t dirty : 1;         
-    uint64_t size : 1;          
-    uint64_t reserved1 : 4;
-    uint64_t physicalAddress : 40;
-    uint64_t reserved2 : 11;
-    uint64_t executeDisable : 1;
+    ulong present : 1;
+    ulong readWrite : 1;
+    ulong userSupervisor : 1;
+    ulong writeThrough : 1;
+    ulong cacheDisable : 1;
+    ulong accessed : 1;
+    ulong dirty : 1;
+    ulong size : 1;
+    ulong reserved1 : 4;
+    ulong physicalAddress : 40;
+    ulong reserved2 : 11;
+    ulong executeDisable : 1;
+
+    ulong getPhysicalAddress() {
+        return physicalAddress << 12;
+    }
 }
 
-alias PTEntries = PTEntry[PAGE_SIZE];
-alias PDEntries = PDEntry[PAGE_SIZE];
-alias PDPTEntries = PDPTEntry[PAGE_SIZE];
-alias PML4Entries = PML4Entries[PAGE_SIZE];
+struct VirtualAddress {
+    ulong pml4Index : 9;
+    ulong pdptIndex : 9;
+    ulong pdIndex : 9;
+    ulong ptIndex : 9;
+    ulong offset : 12;
 
-alias Level1Mapping = Tuple!(PTEntries, PDEntry);
-alias Level2Mapping = Tuple!(PDEntries, PDPTEntry);
-alias Level3Mapping = Tuple!(PDPTEntries, PML4Entry);
-alias Level4Mapping = Tuple!(Level1Mapping, Level2Mapping, Level3Mapping, PML4Entries);
-
-
-
-
-
-
-
-
-
+    ulong toPhysicalAddress(PTEntry ptEntry) {
+        return (ptEntry.getPhysicalAddress() & ~0xFFFULL) | offset;
+    }
+}
 
