e76edb56589030db705f0038293fd97e2304ae20
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-31 16:57:20 +0330

    Added: segment descriptor definition

--- memory.c
+++ memory.c
@@ -6,3 +6,19 @@ struct SegmentSelector {
     uint16_t privilege_level : 2;  
     uint16_t table_indicator : 1;
 };
+
+struct SegmentDescriptor {
+    uint64_t limit_low : 16;       
+    uint64_t base_low : 24;        
+    uint64_t type : 4;             
+    uint64_t sys : 1;                
+    uint64_t priv : 2;              
+    uint64_t present : 1;          
+    uint64_t limit_high : 4;       
+    uint64_t avail : 1;              
+    uint64_t lng : 1;                
+    uint64_t dflsize : 1;              
+    uint64_t granularity : 1;      
+    uint64_t base_high : 8;        
+} __attribute__((packed));
+
